d693b7f5120b6e193ec39051bd54ec2039700d96
monicabocelli <monica.bocelli93@gmail.com>
2017-01-20 12:35:25 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 var dots = [];
-
+var value = deviceShaken();
 //var value = 0;  //starting value of earthquake
 
 
@@ -19,7 +19,7 @@ function draw(){
      noStroke();
      text("SHAKE YOUR DEVICE", width/2,height - height/1.1);    
     
-    var value = deviceShaken();
+   // var value = deviceShaken();
     var magnitude = int(map(value, 0, 500, 0, 10)); 
     
     if (value > 0){
