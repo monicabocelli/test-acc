50fff47fa8e4bc55e7792b7925769f52c9a0089f
monicabocelli <monica.bocelli93@gmail.com>
2017-01-20 11:35:18 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -21,7 +21,7 @@ function draw(){
      noStroke();
      text("SHAKE YOUR DEVICE", width/2,height - height/1.1);    
     
-    value = deviceShaken(acc);
+    var value = deviceShaken(acc);
     var magnitude = int(map(value, 0, 500, 0, 10)); 
     
     if (value > 0){
